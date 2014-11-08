beb9194ae3e7415dba2285d603a2a1879ffc5ac9
Saket Satta <ssatta_git@yahoo.com>
2014-11-08 15:09:51 -0800

    added text to welcome screen

--- main/java/com/example/sattas/myapplication/MainActivity.java
+++ main/java/com/example/sattas/myapplication/MainActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +15,16 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.home_okay_btn);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                TextView titletext = (TextView) findViewById(R.id.welcomeTitle);
+                titletext.setText("Clicked!");
+            }
+        });
     }
 
 
